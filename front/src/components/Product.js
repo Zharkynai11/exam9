ddc532e8d1ee1f8b4b74ad97a1c8bf8ecdf37c11
Zharkynai <45753210+Zharkynai11@users.noreply.github.com>
2019-04-06 20:02:17 +0300

    Update Product.js

--- front/src/components/Product.js
+++ front/src/components/Product.js
@@ -1,36 +1,36 @@
-import React,{ Component } from 'react';
+import React,{ Component, Fragment} from 'react';
 import axios from 'axios';
 import { throws } from 'assert';
-
 class Product extends Component{
     state = {
-        title: this.props.title,
-        description: this.props.description,
-        date: this.props.date,
-        categories: this.props.categories,
-        price: this.props.price,
-        images: this.props.image
+        }
+    constructor(){
+      super();
+      this.image_list = [];
     }
-    
     componentDidMount() {
       let prod_id = this.props.match.url.split('/')[2];
         axios.get('http://127.0.0.1:8000/api/v1/products/'+prod_id).then(posts => {   
-          
           let prod=posts.data;
-          console.log(prod);
           let categories =''
           for (let cat of prod.category){
             categories += cat.title+' ';
           }
+          for (let photo of prod.photoes){
+            this.image_list.push(photo.image)
+          }
+          if (this.image_list.length==0) this.image_list=['http://denrakaev.com/wp-content/uploads/2015/03/no-image.png'];
           this.setState({title: prod.title,
             description: prod.description,
             date: prod.begin_date,
             categories: categories,
             price: prod.price,
-            images: prod.image})
+            photoes: this.image_list})
         }).catch(error => {
           console.log(error);
         })
+        
+        console.log(this.image_list);
       }
     
     render() {
@@ -42,8 +42,11 @@ class Product extends Component{
             <pre className='product'>{this.state.description}</pre>
             <p>Дата поступления: {this.state.date}</p>
             <p>Цена: {this.state.price}$</p>
-            <img className='product' src={this.state.image} alt = 'image' />
-            
+            {this.image_list.map(image => {
+                    return <Fragment key = {image}>
+                      <img src={image} className='prod_list'></img>
+                    </Fragment>
+                })}
             
         </div>
     );
